964d18be171fef8eec073d5b1384cddfd5baf61c
Jibin Mathews <jibinmathews7@gmail.com>
2017-12-17 09:50:04 +0530

    Removing watchers

--- config/production.js
+++ config/production.js
@@ -1,27 +1,28 @@
 'use strict';
- const path = require('path');
- const rootPath = path.join(__dirname, "..");
+const path = require('path');
+const rootPath = path.join(__dirname, '..');
 
- module.exports = {
-     root: rootPath,
-     db: {
-          uri: "mongodb://127.0.0.1/Hackfest",
-          options: {
-             server: {
-                socketOptions: {
-                     keepAlive: 1
-                 }
-             }
-         }
-     },
-     server: {
-         port: Number(process.env.PORT || 3000)
-     },
-     secret:{
-        jwt: "KC-)&DpEH@e9y6s109!d4VpTCo_V8N"
+module.exports = {
+  root: rootPath,
+  db: {
+    uri: 'mongodb://127.0.0.1/Hackfest',
+    options: {
+      server: {
+        socketOptions: {
+          keepAlive: 1,
+        },
+      },
     },
-    firebase: {
-        apiKey: "AAAAIp9DcdA:APA91bEmSer9HJEADyJqkiTfaeKmzn-kz5ydk08wy8MGjSoBbKZJrBoqE-Dxf9-07jt4YH_0zIPzJfFarUnXxta9pGAi4yxPKlNPtCdqocd7QZHDKttIyyFJrtUTCbJ5tNsCXR5NSzUV",
-        senderId: "148700885456"
-    }
- };
+  },
+  server: {
+    port: Number(process.env.PORT || 3000),
+  },
+  secret: {
+    jwt: 'KC-)&DpEH@e9y6s109!d4VpTCo_V8N',
+  },
+  firebase: {
+    apiKey:
+      'AAAAIp9DcdA:APA91bEmSer9HJEADyJqkiTfaeKmzn-kz5ydk08wy8MGjSoBbKZJrBoqE-Dxf9-07jt4YH_0zIPzJfFarUnXxta9pGAi4yxPKlNPtCdqocd7QZHDKttIyyFJrtUTCbJ5tNsCXR5NSzUV',
+    senderId: '148700885456',
+  },
+};
--- server.js
+++ server.js
@@ -14,119 +14,117 @@ let workerCount = 0;
 const serverPort = config.server.port;
 
 let ipAddr = 'localhost';
-if(process.env.NODE_ENV==="production"){
+if (process.env.NODE_ENV === 'production') {
   ipAddr = 'localhost';
 }
 
-exports.start = (isTest)=>{
-  if(isTest===undefined) {
-    config.db.uri = config.db.uri+"_test";
+exports.start = isTest => {
+  if (isTest === undefined) {
+    config.db.uri = config.db.uri + '_test';
   }
   app = require('./framework/bootstrap')(config);
 
   let server = http.Server(app);
-  require(path.join(__dirname, "socketio"))(server);
+  require(path.join(__dirname, 'socketio'))(server);
 
   server.listen(serverPort, function() {
-    console.log(chalk.red.bold("Server running at port: "+serverPort));
+    console.log(chalk.red.bold('Server running at port: ' + serverPort));
   });
 
-
-
   process.on('message', function(message) {
-    if(message.command === 'shutdown' && message.from === 'master') {
+    if (message.command === 'shutdown' && message.from === 'master') {
       process.exit(0);
     }
   });
 };
 
-exports.close = ()=> {
+exports.close = () => {
   app.close();
 };
 
+exports.start();
 
-process.on('unhandledRejection', (reason, p) => {
-  console.log(chalk.red("Unhandled Rejection at: Promise "), p, chalk.red(" reason: "), chalk.red(reason));
-});
+// process.on('unhandledRejection', (reason, p) => {
+//   console.log(chalk.red("Unhandled Rejection at: Promise "), p, chalk.red(" reason: "), chalk.red(reason));
+// });
 
-process.on('unhandledException', (error, m)=> {
-  console.log(chalk.red("Unhandled Exception at: Error "), m, chalk.red(" reason: "), chalk.red(error));
-})
+// process.on('unhandledException', (error, m)=> {
+//   console.log(chalk.red("Unhandled Exception at: Error "), m, chalk.red(" reason: "), chalk.red(error));
+// })
 
-function createWorkers() {
-  if(workerCount<=0) {
-    return;
-  }else{
-    cluster.fork();
-    workerCount--;
-    setTimeout(createWorkers, 2*1000);
-  }
-}
+// function createWorkers() {
+//   if(workerCount<=0) {
+//     return;
+//   }else{
+//     cluster.fork();
+//     workerCount--;
+//     setTimeout(createWorkers, 2*1000);
+//   }
+// }
 
-if(cluster.isMaster && (process.env.NODE_ENV==="production" || process.argv.indexOf("--force-cluster")!==-1)) {
-  // Watcher for no downtime updatation
-  chokidar.watch('.', {
-    ignored: ['tmp/*', 'public/*'],
-    persistent: true,
-    ignoreInitial: true,
-    cwd: '.',
-    depth: 99,
-    interval: 10000
-  }).on('all', restartWorkers);
+// if(cluster.isMaster && (process.env.NODE_ENV==="production" || process.argv.indexOf("--force-cluster")!==-1)) {
+//   // Watcher for no downtime updatation
+//   chokidar.watch('.', {
+//     ignored: ['tmp/*', 'public/*'],
+//     persistent: true,
+//     ignoreInitial: true,
+//     cwd: '.',
+//     depth: 99,
+//     interval: 10000
+//   }).on('all', restartWorkers);
 
+//   if(numWorkers === 1) {
+//     numWorkers *= 2;
+//   }
 
-  if(numWorkers === 1) {
-    numWorkers *= 2;
-  }
+//   console.log(chalk.yellow("Master setting up "+numWorkers+" workers"));
+//   workerCount = numWorkers;
 
-  console.log(chalk.yellow("Master setting up "+numWorkers+" workers"));
-  workerCount = numWorkers;
+//   createWorkers();
 
-  createWorkers();
+//   cluster.on('online', (worker)=>{
+//     console.log(chalk.magenta("Worker thread: "+worker.process.pid+" is online"));
+//   });
+//   cluster.on('exit', (worker, code, signal)=>{
+//     console.log(chalk.red('Worker thread: '+worker.process.pid+" is exiting"));
+//     cluster.fork();
+//   });
+// }else{
+//   exports.start(false);
+// }
 
-  cluster.on('online', (worker)=>{
-    console.log(chalk.magenta("Worker thread: "+worker.process.pid+" is online"));
-  });
-  cluster.on('exit', (worker, code, signal)=>{
-    console.log(chalk.red('Worker thread: '+worker.process.pid+" is exiting"));
-    cluster.fork();
-  });
-}else{
-  exports.start(false);
-}
+// let workerIds = [];
+// let appRestarting = false;
 
-let workerIds = [];
-let appRestarting = false;
+// function restartWorkers(event, path) {
+//   if(appRestarting) {
+//     return;
+//   }
+//   for(let wid in cluster.workers) {
+//     workerIds.push(wid)
+//   }
+//   appRestarting = true;
+//   setTimeout(()=>{
+//     appRestarting = false;
+//   }, numWorkers*1000*5);
+//   stopWorker();
+// }
 
-function restartWorkers(event, path) {
-  if(appRestarting) {
-    return;
-  }
-  for(let wid in cluster.workers) {
-    workerIds.push(wid)
-  }
-  appRestarting = true;
-  setTimeout(()=>{
-    appRestarting = false;
-  }, numWorkers*1000*5);
-  stopWorker();
-}
-
-function stopWorker() {
-  if(workerIds.length<=0) {
-    return;
-  }
-  if(Object.keys(cluster.workers).length>0) {
-    let wid = workerIds.pop();
-    cluster.workers[wid].send({
-      command: 'shutdown',
-      from: 'master'
-    });
-    setTimeout(()=>{
-      if(cluster.workers[wid]) {
-        cluster.workers[wid].process.kill('SIGKILL');
-      }
-    }, 20*1000);
-  }
-  setTimeout(stopWorker, 5*1000);
-}
+// function stopWorker() {
+//   if(workerIds.length<=0) {
+//     return;
+//   }
+//   if(Object.keys(cluster.workers).length>0) {
+//     let wid = workerIds.pop();
+//     cluster.workers[wid].send({
+//       command: 'shutdown',
+//       from: 'master'
+//     });
+//     setTimeout(()=>{
+//       if(cluster.workers[wid]) {
+//         cluster.workers[wid].process.kill('SIGKILL');
+//       }
+//     }, 20*1000);
+//   }
+//   setTimeout(stopWorker, 5*1000);
+// }
